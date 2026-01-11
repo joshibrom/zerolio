e482ada4ef4d8bcad75c003c4ff5d53d4a6a981b
Joshua Ibrom <joshuaibrom@gmail.com>
2026-01-11 14:05:12 -0600

    feat: read directory listings and files
    Can read listings of directories to store filenames into arrays for
    later use and read files in their entirety to a single string. Care has
    been taken to ensure that there are no memory leaks.

---
+++ src/filesystem.zig
@@ -0,0 +1,95 @@
+const std = @import("std");
+
+const Allocator = std.mem.Allocator;
+
+const EMPLOYMENT_DIRNAME = "employment";
+const PROJECTS_DIRNAME = "projects";
+
+/// Holds a listing of filenames and their paths from a content directory.
+pub const ContentListing = struct {
+    const Self = @This();
+
+    employment: []const []const u8,
+    projects: []const []const u8,
+
+    /// Scans a provided content directory for a listing of employment and
+    /// project HTML files.
+    pub fn fromContentDir(allocator: Allocator, path: []const u8) !Self {
+        var content_dir = std.fs.cwd().openDir(path, .{ .iterate = true }) catch |err| switch (err) {
+            error.FileNotFound => return error.NoContentDirectory,
+            else => return err,
+        };
+        defer content_dir.close();
+
+        const employment_path =
+            try std.fmt.allocPrint(allocator, "{s}/{s}", .{ path, EMPLOYMENT_DIRNAME });
+        defer allocator.free(employment_path);
+        var employment_dir = content_dir.openDir(EMPLOYMENT_DIRNAME, .{ .iterate = true }) catch |err| switch (err) {
+            error.FileNotFound => return error.NoEmploymentDirectory,
+            else => return err,
+        };
+        defer employment_dir.close();
+
+        const projects_path =
+            try std.fmt.allocPrint(allocator, "{s}/{s}", .{ path, PROJECTS_DIRNAME });
+        defer allocator.free(projects_path);
+        var projects_dir = content_dir.openDir(PROJECTS_DIRNAME, .{ .iterate = true }) catch |err| switch (err) {
+            error.FileNotFound => return error.NoProjectsDirectory,
+            else => return err,
+        };
+        defer projects_dir.close();
+
+        const employment_files = try Self.scanDir(
+            allocator,
+            &employment_dir,
+            employment_path,
+        );
+        const projects_files = try Self.scanDir(
+            allocator,
+            &projects_dir,
+            projects_path,
+        );
+
+        return Self{
+            .employment = employment_files,
+            .projects = projects_files,
+        };
+    }
+
+    /// Scans an individual directory for the HTML files in that directory.
+    fn scanDir(allocator: Allocator, dir: *const std.fs.Dir, path: []const u8) ![][]const u8 {
+        var listing: std.ArrayList([]const u8) = .empty;
+        var walker = try dir.walk(allocator);
+        defer walker.deinit();
+        while (try walker.next()) |entry| {
+            if (std.mem.endsWith(u8, entry.basename, ".html")) {
+                const fname = try allocator.dupe(u8, entry.basename);
+                defer allocator.free(fname);
+                const fpath = try std.fmt.allocPrint(allocator, "{s}/{s}", .{ path, fname });
+                try listing.append(allocator, fpath);
+            }
+        }
+        return listing.toOwnedSlice(allocator);
+    }
+
+    pub fn deinit(self: Self, allocator: Allocator) void {
+        for (self.employment) |item| {
+            allocator.free(item);
+        }
+        allocator.free(self.employment);
+
+        for (self.projects) |item| {
+            allocator.free(item);
+        }
+        allocator.free(self.projects);
+    }
+};
+
+/// Loads a file in its entirety into a string. Really just a wrapper for
+/// `std.fs.cwd().readFileAlloc` with a 16MB max read size.
+///
+/// Returns `error.FileTooBig` if the file is larger than 16MB in size.
+pub fn load_file(allocator: Allocator, path: []const u8) ![]const u8 {
+    const max_bytes = 16 * 1024 * 1024;
+    return std.fs.cwd().readFileAlloc(allocator, path, max_bytes);
+}
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,37 @@
 const std = @import("std");
+const filesystem = @import("filesystem.zig");
 
-pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+pub fn main() void {
+    var gpa = std.heap.DebugAllocator(.{}).init;
+    defer if (gpa.deinit() == .leak) std.debug.panic("[E] Memory leak detected.\n", .{});
+    const allocator = gpa.allocator();
+
+    var stderr_buf: [1024]u8 = undefined;
+    var stderr_writer = std.fs.File.stderr().writer(&stderr_buf);
+    const stderr = &stderr_writer.interface;
+
+    const content_listing = get_content_dir_files(allocator, stderr);
+    defer content_listing.deinit(allocator);
+
+    for (content_listing.employment) |f| {
+        const file_content = filesystem.load_file(allocator, f) catch @panic("uh oh");
+        defer allocator.free(file_content);
+        std.debug.print("{s}\n", .{file_content});
+    }
+}
+
+/// Helper function to get filenames from content directory and handle errors.
+/// This function is primarily intended to ensure that the main function does
+/// not become too cluttered and enforces separation of concerns.
+fn get_content_dir_files(allocator: std.mem.Allocator, stderr: *std.io.Writer) filesystem.ContentListing {
+    return filesystem.ContentListing.fromContentDir(allocator, "content") catch |err| {
+        switch (err) {
+            error.NoContentDirectory => stderr.print("[E] Content directory does not exist!\n", .{}) catch {},
+            error.NoEmploymentDirectory => stderr.print("[E] Employment directory does not exist!\n", .{}) catch {},
+            error.NoProjectsDirectory => stderr.print("[E] Projects directory does not exist!\n", .{}) catch {},
+            else => stderr.print("[E] Error when scanning content directory: {}\n", .{err}) catch {},
+        }
+        stderr.flush() catch {};
+        std.process.exit(1);
+    };
 }
