e921696b939a392b9399c27bceab47d4e1007e73
Joshua Ibrom <joshuaibrom@gmail.com>
2026-01-11 18:06:47 -0600

    feat: use arena allocator to ensure all memory freed
    My previous commit did appear to correctly handle memory lifetimes such
    that there were no memory leaks, but I'd like to use an arena allocator
    to ensure that this is true. This also means that I can increase
    performance and ensure that memory is freed when it is no longer needed.

--- src/main.zig
+++ src/main.zig
@@ -5,28 +5,34 @@ const parser = @import("parser.zig");
 pub fn main() void {
     var gpa = std.heap.DebugAllocator(.{}).init;
     defer if (gpa.deinit() == .leak) std.debug.panic("[E] Memory leak detected.\n", .{});
-    const allocator = gpa.allocator();
+    const gpa_allocator = gpa.allocator();
 
     var stderr_buf: [1024]u8 = undefined;
     var stderr_writer = std.fs.File.stderr().writer(&stderr_buf);
     const stderr = &stderr_writer.interface;
 
-    const content_listing = get_content_dir_files(allocator, stderr);
-    defer content_listing.deinit(allocator);
+    const content_listing = get_content_dir_files(gpa_allocator, stderr);
+    defer content_listing.deinit(gpa_allocator);
+
+    var arena = std.heap.ArenaAllocator.init(gpa_allocator);
+    defer if (!arena.reset(.free_all)) @panic("Could not free all memory."); // FIXME: Update method of panicking
+    const arena_allocator = arena.allocator();
 
     for (content_listing.employment) |f| {
-        const file_content = filesystem.load_file(allocator, f) catch @panic("uh oh");
-        defer allocator.free(file_content);
-        const entry = parser.parseEntry(allocator, file_content, .Employment) catch @panic("There was an issue parsing bro");
-        defer entry.deinit(allocator);
+        const file_content = filesystem.load_file(arena_allocator, f) catch @panic("uh oh");
+        defer arena_allocator.free(file_content);
+        const entry = parser.parseEntry(arena_allocator, file_content, .Employment) catch @panic("There was an issue parsing bro");
+        defer entry.deinit(arena_allocator);
         std.debug.print("title: {s}\n", .{entry.title});
         std.debug.print("{s}\n", .{entry.text});
     }
+    // NOTE: I would like to use an arena so that each entry type resets all memory when no longer needed
+    if (!arena.reset(.free_all)) @panic("Could not free all memory."); // FIXME: Update method of panicking
     for (content_listing.projects) |f| {
-        const file_content = filesystem.load_file(allocator, f) catch @panic("uh oh");
-        defer allocator.free(file_content);
-        const entry = parser.parseEntry(allocator, file_content, .Project) catch @panic("There was an issue parsing bro");
-        defer entry.deinit(allocator);
+        const file_content = filesystem.load_file(arena_allocator, f) catch @panic("uh oh");
+        defer arena_allocator.free(file_content);
+        const entry = parser.parseEntry(arena_allocator, file_content, .Project) catch @panic("There was an issue parsing bro");
+        defer entry.deinit(arena_allocator);
         std.debug.print("title: {s}\n", .{entry.title});
         for (entry.data.Project.tags) |t| {
             std.debug.print("  - '{s}'\n", .{t});
