01614740a4427154faca23fb60d03678e71607d7
Joshua Ibrom <joshuaibrom@gmail.com>
2026-01-13 22:18:08 -0600

    chore: move templates to own directory
    I still need to figure out where I'd like to place partials that render
    specific page components.

--- src/renderer.zig
+++ src/renderer.zig
@@ -6,13 +6,13 @@ const Allocator = std.mem.Allocator;
 
 const Replacement = struct { []const u8, []const u8 };
 
-const BASE_LAYOUT_PATH = "content/layouts/base.html";
+const BASE_LAYOUT_PATH = "templates/layouts/base.html";
 
 pub fn renderIndexPage(gpa: Allocator) ![]const u8 {
     var arena = std.heap.ArenaAllocator.init(gpa);
     defer arena.deinit();
     const allocator = arena.allocator();
-    const template = try filesystem.load_file(allocator, "content/views/index.html");
+    const template = try filesystem.load_file(allocator, "templates/views/index.html");
     // TODO: Render template parts before finalizing page
     const base_layout = try filesystem.load_file(allocator, BASE_LAYOUT_PATH);
     const replacements = [_]Replacement{
